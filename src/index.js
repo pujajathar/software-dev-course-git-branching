b947c5bdb2ad22fe12e0a541573b369dcfaac584
pujajathar <pujajathar91@.com>
2026-05-17 23:06:07 -0500

    Add commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-console.log("Hello world!");
 
+//javascript
+   // Puja Jathar - 05/17/2026
+
+console.log("Hello world!");
+console.log("Numbers: ");
 for (let i = 0; i < 10; i++) {
     console.log(i);
 }
